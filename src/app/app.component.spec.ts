e0f1fcddf901967ae69c65f7560c3ac5bc9dec45
Jan Okularczyk <jan.okularczyk@capgemini.com>
2023-02-14 11:28:31 +0100

    Rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ShoppingListApp'`, () => {
+  it(`should have as title 'RecipeBookApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ShoppingListApp');
+    expect(app.title).toEqual('RecipeBookApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ShoppingListApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('RecipeBookApp app is running!');
   });
 });
