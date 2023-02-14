c054fa9790078e415eb062b0dc9db9b421a6f645
Jan Okularczyk <jan.okularczyk@capgemini.com>
2023-02-14 12:22:44 +0100

    Displaying recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('test', 'test recipe', 'https://images.pexels.com/photos/1640771/pexels-photo-1640771.jpeg?auto=compress&cs=tinysrgb&w=600')
+    new Recipe('test', 'test recipe', 'https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&w=600')
   ];
 
   constructor() { }
