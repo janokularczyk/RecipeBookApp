cce53c2604d10057e2b6df0ca059e1ecf4ddf7fb
Jan Okularczyk <jan.okularczyk@capgemini.com>
2023-02-16 15:24:53 +0100

    Fix service push

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('test', 5),
         new Ingredient('test', 2),
@@ -12,5 +14,6 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
